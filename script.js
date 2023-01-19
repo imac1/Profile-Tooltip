871fd086d93bbbfd7d57eddaf5df54eaa29c26ba
imac1 <irina.macovei@yahoo.fr>
2023-01-19 20:36:20 +0200

    semifinal

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-document.getElementById("white-button").addEventListener("click", function (e) {
-	let cardsContainer = document.getElementById("cards-container");
-	let newDarkBox = document.createElement("div");
-	newDarkBox.classList.add(".box-dark");
-	cardsContainer.appendChild(newDarkBox);
-	console.log(newDarkBox);
+let messButton = document.querySelector("button");
+
+messButton.addEventListener("click", function (event) {
+	// Select the first box-1 element
+	let firstBox1 = document.querySelector(".box-white");
+
+	// Create a new div element for the additional box-1
+	let newBox1 = document.createElement("div");
+
+	// Add the class "box-1" to the new div element
+	newBox1.classList.add("box-white");
+
+	// Insert the new box-1 element after the first box-1
+	firstBox1.insertAdjacentElement("afterend", newBox1);
 });
